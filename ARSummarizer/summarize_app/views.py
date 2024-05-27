4ee5f9eb092f2dd1a271b4c0c27592fc321c54c3
ayushmittal77 <ayushmittal77@gmail.com>
2024-05-27 22:30:52 +0530

    updated views.py and ARSummarizer/urls.py. Created urls.py in summarize_app

--- ARSummarizer/summarize_app/views.py
+++ ARSummarizer/summarize_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+from . import serializer
+
+
+class DocumentUploadAPIView(APIView):
+    
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+
+        file_serializer = serializer.DocumentUploadSerializer(data=request.data)
+
+        if file_serializer.is_valid():
+            file = request.FILES['file']
+            file_content = file.read()
+            file_extension = file.name.split('.')[-1].lower()
+
+        return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
